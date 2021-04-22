62c24123e9f0092969eb017fd159d0eb7c2e6291
Eduardo Hinojosa <30810344+FS-Frost@users.noreply.github.com>
2021-04-22 17:00:00 -0400

    Remove log.

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -7,8 +7,6 @@ const reviver = (_: string, value: any) => (isNaN(value) ? value : value - 0);
 const componentResolver = (name: string) =>
     import(`./components/${name}`).then((module) => module.default);
 
-console.log("app");
-
 render(
     <InertiaApp
         initialPage={app ? JSON.parse(app.dataset.page!, reviver) : "{}"}
