0f8f2f3f287c3071e5122e8fe7a41a3da64cb7c7
Eduardo Hinojosa <30810344+FS-Frost@users.noreply.github.com>
2021-04-22 17:08:23 -0400

    Remove reviver on initial render.

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,13 +3,12 @@ import { render } from "react-dom";
 import { InertiaApp } from "@inertiajs/inertia-react";
 
 const app = document.getElementById("app");
-const reviver = (_: string, value: any) => (isNaN(value) ? value : value - 0);
 const componentResolver = (name: string) =>
     import(`./components/${name}`).then((module) => module.default);
 
 render(
     <InertiaApp
-        initialPage={app ? JSON.parse(app.dataset.page!, reviver) : "{}"}
+        initialPage={app ? JSON.parse(app.dataset.page!) : "{}"}
         resolveComponent={componentResolver}
     />,
     app
